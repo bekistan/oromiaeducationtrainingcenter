431311c0a99ab616c182cf595caca9660060cf01
beki Tsegaye <bekitsegaye9@gmail.com>
2025-06-28 12:41:28 +0000

    no the logo is in the public/image/logo.png use the file from here

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -5,7 +5,7 @@ export function Logo() {
   return (
     <Link href="/" className="flex items-center space-x-2" aria-label="Homepage">
       <Image
-        src="https://i.ibb.co/3k5gNTd/logo.png"
+        src="/images/logo.png"
         alt="Oromia Education Training Center Logo"
         width={281}
         height={214}
